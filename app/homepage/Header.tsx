d1484ea284ac5810db104f3fd91910ab217f3ed1
ThuyQuynh140203 <nthuyquynh260@gmail.com>
2024-10-13 09:15:06 +0700

    Update Header

--- app/homepage/Header.tsx
+++ app/homepage/Header.tsx
@@ -89,6 +89,23 @@ function Header() {
                 </div>
               )}
             </div>
+            <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
