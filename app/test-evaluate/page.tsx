9d7c0c42628389f689afc8f74f0193b693539484
duyen3923 <riel3923@gmail.com>
2024-10-13 21:12:58 +0700

    .

---
+++ app/test-evaluate/page.tsx
@@ -0,0 +1,15 @@
+import DefaultLayout from '@/components/Layouts/defaultLayout'
+import React from 'react'
+import ListEvaluate from './listEvaluate'
+
+const Evaluate = () => {
+  return (
+    <div>
+   <DefaultLayout>
+      <ListEvaluate />
+    </DefaultLayout>
+    </div>
+  )
+}
+
+export default Evaluate
