16c4d923783317d7aed18d4ea967f73d5a5af6e7
ThuyQuynh140203 <94602621+ThuyQuynh140203@users.noreply.github.com>
2024-11-10 22:11:29 +0700

    Update file page show faculty user

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -1,37 +1,5 @@
-
-'use client';
-import React, { useState, useEffect } from 'react';
-import Header from '../homepage/Header';
-import Footer from '../homepage/Footer';
-import { Button } from '@/components/ui/button';
-
-interface Faculty {
-  id: string;
-  name: string;
-  description?: string;
-}
-
-const Page = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [faculties, setFaculties] = useState<Faculty[]>([]);
-
-  useEffect(() => {
-    const fetchFaculties = async () => {
-      try {
-        const response = await fetch('/api/faculty');
-        const data = await response.json();
-        setFaculties(data || []);
-      } catch (error) {
-        console.log(error);
-        setFaculties([]);
-      }
-    };
-    fetchFaculties();
-  }, []);
-
-  const filteredFaculties = faculties.filter((faculty) =>
-    faculty.name.toLowerCase().includes(searchQuery.toLowerCase()),
-  );
+import React from 'react';
+import FacultyPage from './FacultyPage';
 
 const page = () => {
   return (
