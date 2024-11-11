3b0db25cf6ade176218391ea36077fbb7ac70790
duyen3923 <riel3923@gmail.com>
2024-11-11 17:08:42 +0700

    .

--- app/patients/[userId]/profile/page.tsx
+++ app/patients/[userId]/profile/page.tsx
@@ -1,21 +1,29 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { RiEmpathizeFill, RiMoneyDollarCircleLine } from 'react-icons/ri';
+import { Button } from '@/components/ui/button';
+import { FaBookMedical, FaNewspaper } from 'react-icons/fa';
+import {
+  Cake,
+  CircleDot,
+  CircleUser,
+  FlipVertical2,
+  Mails,
+  PencilLine,
+  Smartphone,
+  Trash2,
+} from 'lucide-react';
+import Link from 'next/link';
+import Header from '@/app/homepage/Header';
+import MedicalRecord from './MedicalRecord';
+import Footer from '@/app/homepage/Footer';
+import PaymentHistory from './PaymentHistory';
 
-"use client";
-import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import { RiEmpathizeFill, RiMoneyDollarCircleLine } from "react-icons/ri";
-import { Button } from "@/components/ui/button";
-import { FaBookMedical, FaNewspaper } from "react-icons/fa";
-import { Cake, PencilLine, Smartphone, Trash2, UserRoundPen } from "lucide-react";
-import Link from "next/link";
-import Header from "@/app/homepage/Header";
-import MedicalRecord from "./MedicalRecord";
-import Footer from "@/app/homepage/Footer";
-import PaymentHistory from "./PaymentHistory";
-
-import Modal from "@/components/Modal";
-import { toast } from "sonner";
-import ProfileDetailModal from "@/app/patients/[userId]/profile/ProfileDetailModal";
-import type { Profile } from "@/types/interface";
+import Modal from '@/components/Modal';
+import { toast } from 'sonner';
+import ProfileDetailModal from '@/app/patients/[userId]/profile/ProfileDetailModal';
+import type { Profile } from '@/types/interface';
 
 const Profile = () => {
   const [selectedOption, setSelectedOption] = useState(1);
@@ -39,11 +47,7 @@ const Profile = () => {
           throw new Error('Lỗi khi lấy dữ liệu hồ sơ');
         }
         const profiles = await response.json();
-        if (Array.isArray(profiles)) {
-          setProfiles(profiles);
-        } else {
-          setProfiles([]);
-        }
+        setProfiles(profiles);
       } catch (error) {
         console.error('Lỗi khi lấy dữ liệu hồ sơ:', error);
       }
@@ -54,7 +58,6 @@ const Profile = () => {
     }
   }, [session]);
 
-  // Hàm xóa hồ sơ
   const handleDeleteProfile = async () => {
     if (profileToDelete === null) return;
 
@@ -70,7 +73,7 @@ const Profile = () => {
       });
 
       if (!response.ok) {
-        toast.error("Lỗi khi xóa hồ sơ");
+        toast.error('Lỗi khi xóa hồ sơ');
       }
 
       setProfiles((prevProfiles) =>
@@ -78,15 +81,15 @@ const Profile = () => {
       );
       toast.success('Xóa hồ sơ thành công');
     } catch (error) {
-      toast.error("Lỗi khi xóa hồ sơ");
+      toast.error('Lỗi khi xóa hồ sơ');
     } finally {
       setIsModalOpen(false);
-      setProfileToDelete(null);     
+      setProfileToDelete(null);
     }
   };
   const handleShowProfile = (profile: Profile) => {
     setSelectedProfile(profile);
-    setIsDetailModalOpen(true); // Mở modal chi tiết
+    setIsDetailModalOpen(true);
   };
 
   const Buttons = [
@@ -106,18 +109,18 @@ const Profile = () => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-full max-w-5xl">
           <div className="rounded-lg flex flex-col items-center p-4 w-full lg:w-auto">
             <Link href={`/patients/${session?.user.id}/profile/add-profile`}>
-              <Button className="justify-start border-slate-300 bg-white hover:bg-sky-200 w-full gap-2 items-center p-3 rounded-lg border-2">
-                <RiEmpathizeFill className="text-blue-400" />
+              <Button className="group justify-start w-full gap-2 items-center p-3 rounded-3xl border-2 border-slate-50 bg-white hover:bg-gradient-to-r hover:from-[#00b5f1] hover:to-[#00e0ff] hover:text-white shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-200 ease-in-out">
+                <RiEmpathizeFill className="text-primary group-hover:text-white" />
                 Thêm hồ sơ khám bệnh
               </Button>
             </Link>
-            <hr className="w-full border-t-2 border-blue-300 mt-4" />
+            <hr className="w-full border-t-2 border-primary mt-4" />
             <ul className="mt-4 w-full">
               {Buttons.map((item) => (
                 <li key={item.id} className="mt-2 w-full">
                   <Button
-                    className={`w-full gap-2 flex items-center justify-start p-3 hover:bg-sky-200 bg-white ${
-                      selectedOption === item.id ? 'bg-blue-50' : ''
+                    className={`w-full gap-2 flex items-center justify-start p-3 hover:bg-gradient-to-r hover:from-[#00b5f1] hover:to-[#00e0ff] bg-white hover:text-white ${
+                      selectedOption === item.id ? 'bg-slate-100 text-primary' : ''
                     }`}
                     onClick={() => setSelectedOption(item.id)}
                   >
@@ -134,51 +137,56 @@ const Profile = () => {
               <div>
                 <p className="text-slate-300 text-xs">PATIENT'S RECORDS</p>
                 <h2 className="text-lg font-bold">Danh sách hồ sơ bệnh nhân</h2>
-                <hr className="w-full border-t-2 border-blue-300 mt-4" />
+                <hr className="w-full border-t-2 border-primary mt-4" />
                 <br />
 
                 {profiles.length > 0 ? (
                   profiles.map((profile) => (
                     <div
                       key={profile.id}
-                      className="bg-slate-50 p-4 text-sm rounded-lg shadow-md transition-all ease-in-out duration-100 cursor-pointer mb-4"
+                      className=" p-4 text-sm rounded-lg shadow-md hover:shadow-lg hover:-translate-y-1 transition-all ease-in-out duration-100 cursor-pointer mb-4"
                     >
                       <div className="flex flex-col gap-4 lg:flex-row lg:gap-8">
                         <div className="rounded-lg bg-gray-200 flex-1">
-                          <p className="text-primary mt-2 text-lg flex items-center gap-2">
-                            <UserRoundPen className="w-5 h-5" />
-                            <span className="text-lg uppercase">{profile.name}</span>
+                          <p className="text-primary mt-2 flex items-center gap-2">
+                            <CircleUser className="w-5 h-5" />
+                            <span className=" uppercase">{profile.name}</span>
                           </p>
-                          <p className="mt-2 text-lg flex items-center gap-2">
-                            <Smartphone className="w-5 h-5" />
-                            <span>{profile.phone}</span>
+                          <p className="mt-2 flex items-center gap-2">
+                            <Mails className="w-5 h-5 text-slate-400" />
+                            <span className="text-slate-400">Email:</span>
+                            <span>{profile.email}</span>
                           </p>
-                          <p className="mt-2 text-lg flex items-center gap-2">
-                            <Cake className="w-5 h-5" />
+                          <p className="mt-2 flex items-center gap-2">
+                            <Smartphone className="w-5 h-5 text-slate-400" />
+                            <span className="text-slate-400">Số điện thoại:</span>
+                            <span>
+                              {profile.phone
+                                ? profile.phone.replace(/(\d{3})\d{4}(\d{3})/, '$1****$2')
+                                : 'N/A'}
+                            </span>
+                          </p>
+                          <p className="mt-2 flex items-center gap-2">
+                            <Cake className="w-5 h-5 text-slate-400" />
+                            <span className="text-slate-400">Ngày sinh:</span>
                             <span>
                               {profile.birthDate
                                 ? new Date(profile.birthDate).toLocaleDateString()
                                 : 'N/A'}
                             </span>
                           </p>
-                        </div>
-
-                        <div className="rounded-lg bg-gray-200 flex-1">
-                          <div className="mt-4 flex justify-end">
-                            <Button 
-                              className="bg-slate-50 text-green-400 hover:bg-green-400 hover:text-white text-sm px-4 py-2"
-                              onClick={() => handleShowProfile(profile)}
-                            >
-                              Xem chi tiết
-                            </Button>
-                          </div>
+                          <p className="mt-2 flex items-center gap-2">
+                            <FlipVertical2 className="w-5 h-5 text-slate-400" />
+                            <span className="text-slate-400">Giới tính:</span>
+                            <span>{profile.gender === 'FEMALE' ? 'Nữ' : 'Nam'}</span>
+                          </p>
                         </div>
                       </div>
 
                       <hr className="mt-2" />
                       <div className="mt-2 flex justify-end gap-2">
                         <Button
-                          className="bg-slate-50 text-red-400 hover:bg-red-400 hover:text-white text-sm"
+                          className="bg-white text-red-400 hover:bg-red-400 hover:text-white text-sm"
                           onClick={() => {
                             setProfileToDelete(profile.id);
                             setIsModalOpen(true);
@@ -187,24 +195,38 @@ const Profile = () => {
                           <Trash2 className="w-4 h-4 inline mr-1" />
                           Xóa hồ sơ
                         </Button>
-                        <Link href={{
-                          pathname:`/patients/${profile.id}/profile/edit-profile`,
-                          query:{id:profile.id,
-                                name:profile.name,
-                                email:profile.email,
-                                phone:profile.phone,
-                                gender:profile.gender,
-                                identificationType:profile.identificationType,
-                                identificationNumber:profile.identificationNumber,
-                                identificationDocumentUrl:profile.identificationDocumentUrl,
-                                pastMedicalHistory:profile.pastMedicalHistory,
-                                birthDate:profile.birthDate ? profile.birthDate.toString() : ""}
-                        }}>
-                          <Button className="bg-slate-50 text-primary hover:bg-primary hover:text-white text-sm">
+                        <Link
+                          href={{
+                            pathname: `/patients/${profile.id}/profile/edit-profile`,
+                            query: {
+                              id: profile.id,
+                              name: profile.name,
+                              email: profile.email,
+                              phone: profile.phone,
+                              gender: profile.gender,
+                              identificationType: profile.identificationType,
+                              identificationNumber: profile.identificationNumber,
+                              identificationDocumentUrl:
+                                profile.identificationDocumentUrl,
+                              pastMedicalHistory: profile.pastMedicalHistory,
+                              birthDate: profile.birthDate
+                                ? profile.birthDate.toString()
+                                : '',
+                            },
+                          }}
+                        >
+                          <Button className="bg-white text-primary hover:bg-primary hover:text-white text-sm">
                             <PencilLine className="w-4 h-4 inline mr-1" />
                             Sửa hồ sơ
                           </Button>
                         </Link>
+                        <Button
+                              className="bg-white text-black hover:bg-slate-400 hover:text-white text-sm px-4 py-2"
+                              onClick={() => handleShowProfile(profile)}
+                            >
+                              <CircleDot className="w-4 h-4 inline mr-1" />
+                              Chi tiết
+                            </Button>
                       </div>
                     </div>
                   ))
@@ -248,12 +270,12 @@ const Profile = () => {
       />
 
       {selectedProfile && (
-        <ProfileDetailModal 
+        <ProfileDetailModal
           isOpen={isDetailModalOpen}
           onClose={() => setIsDetailModalOpen(false)}
           profile={selectedProfile}
         />
-      )}  
+      )}
     </div>
   );
 };
