aafe172b088b71a4ab97fc5914457b90a1ce39df
sherwin-huynh <huynhchitrung020503@gmail.com>
2024-11-20 15:37:33 +0700

    Move doctor to admin

---
+++ app/admin/doctor/page.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from '@/components/Layouts/defaultLayout'
+import ListDoctor from '@/components/ListDoctor'
+import React from 'react'
+
+
+const Doctor = () => {
+  return (
+    <DefaultLayout>
+      <ListDoctor />
+    </DefaultLayout>
+  )
+}
+
+export default Doctor
