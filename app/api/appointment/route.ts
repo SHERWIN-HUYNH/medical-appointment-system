090a1cc7e2a2c2491985e9377a3561201a7b0d60
sherwin-huynh <huynhchitrung020503@gmail.com>
2024-11-20 17:35:27 +0700

    adjust api/appointment

--- app/api/appointment/route.ts
+++ app/api/appointment/route.ts
@@ -1,7 +1,11 @@
-import { successResponse } from '@/helpers/response'
 import { AppointmentRepository } from '@/repositories/appointment'
 
 export async function GET() {
-  const appointments = await AppointmentRepository.getAllAppointments()
-  return successResponse(appointments)
+  try {
+    const appointments = await AppointmentRepository.getAllAppointments();
+    return new Response(JSON.stringify(appointments), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching appointments:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }  
 }
